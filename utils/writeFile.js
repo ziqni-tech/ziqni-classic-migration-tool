0e609748e34cd34995e4fd9eb09aa9c084d7fab7
danilov <danilovwebb@gmail.com>
2023-04-20 10:52:05 +0300

    Refactoring writeFile.js

--- utils/writeFile.js
+++ utils/writeFile.js
@@ -1,13 +1,13 @@
 const fs = require('fs');
 const path = require('path');
 
-function writeFile(entityName, data) {
-  const dataDir = path.join('mutatedData', entityName);
+function writeFile(entityName, fileName, data) {
+  const dataDir = path.join('entitiesData', entityName);
   if (!fs.existsSync(dataDir)) {
     fs.mkdirSync(dataDir, { recursive: true });
   }
 
-  const dataFile = path.join(dataDir, `${entityName}s.json`);
+  const dataFile = path.join(dataDir, `${fileName}.json`);
   fs.writeFileSync(dataFile, JSON.stringify(data));
 
   console.log(`Data saved to filePath - ${dataFile}`);
